d9e2b2953809957a114909da4d8343283d13121c
jeosafa eller <jeosafaeller@gmail.com>
2023-10-05 19:13:43 -0300

    pagina terminada!

--- locacao/locacao.js
+++ locacao/locacao.js
@@ -1,6 +1,7 @@
-function girar(moedaId, btnId){
+function girar(moedaId, btnId, imgId){
 const moeda = document.getElementById(moedaId);
 const btn = document.getElementById(btnId);
+const img = document.getElementById(imgId);
  
 let ladoImg = true;
 
@@ -17,9 +18,9 @@ btn.addEventListener('click', () => {
         moeda.innerHTML= `
         
         <ul class="img-arvores rounded-circle list-unstyled text-white  p-5">
-                <li>Decoração com bolas, laços, flores</li>
-                <li>Inclui iluminação <br> - pisca de led</li>
-                <li>Disponivel nas cores vermelho, dourado, champanhe, rosé</li>
+                <li class="lista_moeda">Decoração com bolas, laços, flores</li>
+                <li class="lista_moeda">Inclui iluminação <br> - pisca de led</li>
+                <li class="lista_moeda">Disponivel nas cores vermelho, dourado, champanhe, rosé</li>
         
         `
 
@@ -28,7 +29,7 @@ btn.addEventListener('click', () => {
         moeda.style.transform = 'rotateY(0deg)';
 
         moeda.innerHTML= `
-        <div id="moeda"> <img class="img-arvores rounded-circle" src="../img/locacao/Natal 2023/arvore de natal aluguel bh azul.png" alt=""></div>
+        <div id="moeda"> <img class="img-arvores rounded-circle" src="../img/locacao/Natal 2023/${imgId}.png" alt=""></div>
         `
 
     }
@@ -38,9 +39,9 @@ btn.addEventListener('click', () => {
     });
 }
 
-    girar("moeda1","btn1");
-    girar("moeda2","btn2");
-    girar("moeda3","btn3");
-    girar("moeda4","btn4");
+    girar("moeda1","btn1","img1");
+    girar("moeda2","btn2","img2");
+    girar("moeda3","btn3","img3");
+    girar("moeda4","btn4","img4");
 
 
